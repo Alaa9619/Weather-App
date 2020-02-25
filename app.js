3f456a20f97bde2bbabba0c4887798807ad80081
alaa khaled mahmoud <38635461+Alaa9619@users.noreply.github.com>
2020-02-25 19:31:03 +0200

    callback chaining is applied to make the geocode and the forecast operates one after another

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 const request = require('request')
 const geocode= require('./utils/geocode')
 const forecast= require('./utils/forecast')
+const address = process.argv[2] // third item in it is the arg given by the user
+// Callback chaining 
+geocode(address, (error, data) =>{
+  if(!address){
+   return console.log('please provide the address first')
+  }else{
+  if(error){ // return allows you to stop when there is an error occures in the geocode
+    return console.log(error)
+  }
+  forecast(data.latitude, data.longitude, (error, forecastData) => { //  because we have the same naming we change it to avoid overwriting
+   if(error){ 
+     return console.log(error)
+   }
+   console.log(data.location)
+   console.log(forecastData)
+  })
+}
+} )
 
-// const url = 'https://api.darksky.net/forecast/c869d2ed3aea4f1ec32c97bfdef39f2d/37.8267'//,-122.4233 (should be added to the url for getting a useful data)
-// request({url:url, json: true}, (error, response) => {// if you have a value of error there is no value of response and vice versa
-//     if(error){ //error occures when there is no internet connection
-//         console.log('unable to connect to weather app!')
-
-//     }else if(response.body.error){ //error when we don't provide Latitude
-//         console.log('unable to find location')
-
-//     }else{
-//     console.log(response.body.daily.data[0].summary +' it is currently a '+response.body.currently.temperature+' degreees out. there is a chance '+response.body.currently.precipProbability+' to rain')
-//     }
-// })
-// request({url:mapURL, json:true},(error, response) => {
-//     if(error){
-//         console.log('unable to connect to the services!')
-//     }else if(response.body.features.length === 0){
-//         console.log('unable to find location')
-//     }else{
-//     console.log('log = '+ response.body.features[0].center[0]+' lat ='+response.body.features[0].center[1])
-//     }
-// })
-
-
-
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
