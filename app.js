2cb05dfa567e3edbd12d6c8279522a9773bcabfd
alaa khaled mahmoud <38635461+Alaa9619@users.noreply.github.com>
2020-02-25 20:09:24 +0200

    ES6 feature such as object destructing and property shorthand

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const geocode= require('./utils/geocode')
 const forecast= require('./utils/forecast')
 const address = process.argv[2] // third item in it is the arg given by the user
 // Callback chaining 
-geocode(address, (error, data) =>{
+geocode(address, (error, {latitude, longitude, location}) =>{ // we use destructing code of data here
   if(!address){
    return console.log('please provide the address first')
   }else{
   if(error){ // return allows you to stop when there is an error occures in the geocode
     return console.log(error)
   }
-  forecast(data.latitude, data.longitude, (error, forecastData) => { //  because we have the same naming we change it to avoid overwriting
+  forecast(latitude, longitude, (error, forecastData) => { //  because we have the same naming we change it to avoid overwriting
    if(error){ 
      return console.log(error)
    }
-   console.log(data.location)
+   console.log(location)
    console.log(forecastData)
   })
 }
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,13 +1,13 @@
 const request = require('request')
 const forecast = (latitude, longitude, callback) =>{
 const url = 'https://api.darksky.net/forecast/c869d2ed3aea4f1ec32c97bfdef39f2d/'+ latitude +','+ longitude
-request({url: url, json: true },(error,response)=>{
+request({url, json: true },(error,{body})=>{
     if(error){ //error occures when there is no internet connection
      callback('unable to connect to weather app!', undefined)
-    } else if(response.body.error){ 
+    } else if(body.error){ 
         callback('unable to find location', undefined) //error when we don't provide Latitude
     }else{
-        callback(undefined, response.body.daily.data[0].summary +' it is currently a '+response.body.currently.temperature+' degreees out. there is a chance '+response.body.currently.precipProbability+' to rain')
+        callback(undefined, body.daily.data[0].summary +' it is currently a '+body.currently.temperature+' degreees out. there is a chance '+body.currently.precipProbability+' to rain')
     }
 })
 }
